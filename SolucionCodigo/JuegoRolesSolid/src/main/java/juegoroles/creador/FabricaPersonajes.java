d499f0637aff5d76fedb138a03402ec3e361ef63
ingridmicaa1 <gualanmicaela71@gmail.com>
2026-06-28 02:09:31 -0500

    ClasesFuncionalidad3

--- SolucionCodigo/JuegoRolesSolid/src/main/java/juegoroles/creador/FabricaPersonajes.java
+++ SolucionCodigo/JuegoRolesSolid/src/main/java/juegoroles/creador/FabricaPersonajes.java
@@ -1,11 +1,16 @@
 package juegoroles.creador;
 
+
+import juegoroles.habilidad.DisparoPreciso;
 import juegoroles.habilidad.EscudoDefensivo;
 import juegoroles.habilidad.EspadaCuerpoACuerpo;
 import juegoroles.habilidad.Evadir;
+import juegoroles.habilidad.GolpePoderoso;
 import juegoroles.habilidad.HabilidadAtaque;
 import juegoroles.habilidad.HabilidadDefensa;
+import juegoroles.habilidad.HabilidadEspecial;
 import juegoroles.habilidad.HechizoMagico;
+import juegoroles.habilidad.HechizoSupremo;
 import juegoroles.habilidad.TiroPreciso;
 import juegoroles.personaje.Arquero;
 import juegoroles.personaje.Guerrero;
@@ -13,24 +18,33 @@ import juegoroles.personaje.Mago;
 import juegoroles.personaje.Personaje;
 
 public final class FabricaPersonajes {
+
     private FabricaPersonajes() {
     }
 
     public static Personaje crearGuerrero(String nombre) {
         HabilidadAtaque ataque = new EspadaCuerpoACuerpo();
         HabilidadDefensa defensa = new EscudoDefensivo();
+        HabilidadEspecial especial = new GolpePoderoso();
+
+
         return new Guerrero(nombre, ataque, defensa);
     }
 
     public static Personaje crearMago(String nombre) {
         HabilidadAtaque ataque = new HechizoMagico();
         HabilidadDefensa defensa = new EscudoDefensivo();
+        HabilidadEspecial especial = new HechizoSupremo();
+
         return new Mago(nombre, ataque, defensa);
     }
 
     public static Personaje crearArquero(String nombre) {
         HabilidadAtaque ataque = new TiroPreciso();
         HabilidadDefensa defensa = new Evadir();
+        HabilidadEspecial especial = new DisparoPreciso();
+
         return new Arquero(nombre, ataque, defensa);
     }
 }
+
---
+++ SolucionCodigo/JuegoRolesSolid/src/main/java/juegoroles/habilidad/GolpePoderoso.java
@@ -0,0 +1,32 @@
+
+package juegoroles.habilidad;
+
+
+    public class GolpePoderoso implements HabilidadEspecial {
+
+    private int energia = 100;
+    private int cooldown = 0;
+
+    @Override
+    public void usar() throws Exception {
+
+        if (energia < 20) {
+            throw new Exception("No hay suficiente energía.");
+        }
+
+        if (cooldown > 0) {
+            throw new Exception("La habilidad está en cooldown.");
+        }
+
+        energia -= 20;
+        cooldown = 3;
+
+        System.out.println("Golpe Poderoso ejecutado.");
+    }
+
+    public void reducirCooldown() {
+        if (cooldown > 0) {
+            cooldown--;
+        }
+    }
+}
